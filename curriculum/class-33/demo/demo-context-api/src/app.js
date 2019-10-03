71bbb830629b4cbbc03fdcbeacdecb36875967bc
Lee-Roy King <leeroywking@gmail.com>
2019-10-02 18:47:17 -0700

    updating from upstream

--- curriculum/class-33/demo/demo-context-api/src/app.js
+++ curriculum/class-33/demo/demo-context-api/src/app.js
@@ -12,3 +12,4 @@ export default class App extends React.Component {
     );
   }
 }
+
--- curriculum/class-33/demo/demo-context-api/src/components/editor/functional-context.js
+++ curriculum/class-33/demo/demo-context-api/src/components/editor/functional-context.js
@@ -1,4 +1,4 @@
-import React, {useContext} from 'react';
+import React, { useContext } from 'react';
 import { SettingsContext } from '../settings/site-context.js';
 
 const Content = (props) => {
@@ -9,54 +9,19 @@ const Content = (props) => {
     e.preventDefault();
   };
 
-    return (
-      <>
-        <section>
-          <div>
-            <h1>{context.title}</h1>
-            <div>
-              <a href={`http://www.twitter.com/${context.twitter}`}>
-                @{context.twitter}
-              </a>
-            </div>
-            <div>
-              <a href={`mailto:${context.email}`}>
-                {context.email}
-              </a>
-            </div>
-          </div>
-
-          <form onSubmit={handleSubmit}>
-            <h2>Site Settings</h2>
-            <label>
-              <span>Site Title</span>
-              <input
-                placeholder="Site Title"
-                name="title"
-                onChange={e => context.changeTitleTo(e.target.value)}
-              />
-            </label>
-
-            <label>
-              <span>Twitter Handle</span>
-              <input
-                placeholder="Twitter Handle"
-                name="twitter"
-                onChange={e => context.changeTwitterTo(e.target.value)}
-              />
-            </label>
-
-            <label>
-              <span>Email Address</span>
-              <input
-                placeholder="Email"
-                name="title"
-                onChange={e => context.changeEmailTo(e.target.value)}
-              />
-            </label>
+  return (
+    <div>
+    <h1>{context.title}</h1>
+    <form onSubmit={handleSubmit}>
+      <h2>Site Settings</h2>
+        <span>Site Title</span>
+        <input
+          placeholder="Site Title"
+          name="title"
+          onChange={e => context.changeTitleTo(e.target.value)}
+          />
           </form>
-        </section>
-      </>
+          </div>
     );
 };
 
